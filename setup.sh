f7c1d455ec6d058d54151a20fc2aaa1a6b600cec
TheStupidProwler <alexandre.ouillon@protonmail.com>
2017-01-31 10:27:46 +0100

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Compilation en cours
+clang++ -std=c++11 main.cpp -v
+echo cetait assez inutile mais je suis content
+echo jai pu remplir mon projet
+echo la vie vaut la peine detre vecue
