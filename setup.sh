130b63e8f3e85d07dd05cbd65812541d85cca806
@theStupidProwler <alexandre.ouillon@protonmail.com>
2017-06-09 14:22:22 +0200

    ^-^

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo Compilation en cours ...
 g++ -std=c++11 main.cpp src/templates.cpp
 clang++ -std=c++11 main.cpp src/templates.cpp
-echo cetait assez inutile mais je suis content
+echo c\'etait assez inutile mais je suis content
 echo jai pu remplir mon projet
 echo la vie vaut la peine detre vecue
 echo le fichier est a.out, lancez le avec ./a.out
