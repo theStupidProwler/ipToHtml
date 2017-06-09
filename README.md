08889e66acc8216940352e91e461644c33b582b2
@theStupidProwler <alexandre.ouillon@protonmail.com>
2017-06-09 14:25:56 +0200

    ^-^

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 <h3>Installation:</h3>
 <p>Set your Ip List into:<code>assets/raw.txt</code> and launch setup.sh</p>
 <p>you need to change your templates before you launch a.out</p>
-#Dependances:
 
-<p>All you need is clang++ or g++ </p>
+# Dependances:
+
+<p>All you need is a recent build of clang++ or g++ </p>
 </br>
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "src/templates.hpp"
 using std::ios;
 
- // change this value if you wanna have less/more lines per page
+#define linesPerPage = 255 // change this value if you wanna have less/more lines per page
 
 double nombreDeLignes() //fonction qui compte le nombre de ligne (ca semble logique dit comme ca)
   {
@@ -14,7 +14,7 @@ double nombreDeLignes() //fonction qui compte le nombre de ligne (ca semble logi
     std::ifstream access("assets/raw.txt");
     for(nombreLignes = 0; std::getline(access, contenu); nombreLignes ++){}
     std::cout << "Ip Numbers:" << nombreLignes << std::endl;
-    fichiersAGenerer = nombreLignes / 255;
+    fichiersAGenerer = nombreLignes / linesPerPage;
     std::cout << "Files to create:" << fichiersAGenerer << std::endl;
     return nombreLignes;
 }
@@ -27,7 +27,7 @@ int main()
     int numeroPage, nombreDePages;
     std::string resultat;
     numeroPage = 0;
-    nombreDePages = (nombreDeLignes() / 255);
+    nombreDePages = (nombreDeLignes() / linesPerPage);
     std::ofstream ecriture;
     std::ifstream access("assets/raw.txt");
     std::ofstream annuaire("html/index.html", std::ios::app);
@@ -41,13 +41,13 @@ int main()
         ecriture.seekp(0, std::ios_base::end);
         annuaire << "<li><a href=\"links" << numeroPage << ".html\">" << numeroPage << "</a></li>" << std::endl;
         ecrireDebutPage(resultat);
-        
+
         titleNumber(numeroPage);
 
         h1Number(numeroPage);
         compteARebour = 0;
-        access.seekg(((numeroPage * 255) - 1), ios::beg);
-        while(compteARebour != 255)
+        access.seekg(((numeroPage * linesPerPage) - 1), ios::beg);
+        while(compteARebour != linesPerPage)
           {
             i++;
             compteARebour ++;
